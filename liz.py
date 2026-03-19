55b4c801ae3a45cc0ae8d31e373092f91b006de3
Marília Liz <lizmarilia07@gmail.com>
2026-03-19 11:50:42 -0300

    trace

--- liz.py
+++ liz.py
@@ -1,27 +1,3 @@
-class MiniCPU:
-
-    def __init__(self):
-        self.mem = [0] * 256
-        self.reg = [0, 0, 0, 0]  
-        self.pc = 0
-        self.zf = 0
-        self.running = True
-        self.ciclo = 0
-
-    def fetch(self):
-        op = self.mem[self.pc]
-        a = self.mem[self.pc + 1]
-        b = self.mem[self.pc + 2]
-        self.pc += 3
-        return op, a, b    
-    
-    def run(self):
-        while self.running and self.pc < 256:
-            self.ciclo += 1
-            op, a, b = self.fetch()
-            self.decode_execute(op, a, b)
-            self.trace(op, a, b)
-
 
     #trace()
 
